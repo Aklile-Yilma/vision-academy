65e9834fc5caaee2552b874bd8e6f0f98b970454
aklile-yilma <aklileyilma@gmail.com>
2023-06-17 16:54:30 +0300

    fix(gallery): add width

--- src/components/Gallery/ImageContainer.jsx
+++ src/components/Gallery/ImageContainer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ImageContainer = ({ img }) => {
   return (
-    <div className="flex w-1/3 flex-wrap">
+    <div className="flex w-full lg:w-1/3 flex-wrap">
       <div className="w-full p-1 md:p-2">
         <img
           alt="gallery"
